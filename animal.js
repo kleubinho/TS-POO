94372cbc669ba18a270109ab86efc035ea90e677
Kleber Henrique <yuk9.gabriel@gmail.com>
2023-08-27 21:14:16 -0300

    update

--- animal.js
+++ animal.js
@@ -6,10 +6,30 @@ class Animal {
         this.type = type;
         this.age = age;
     }
+    communicate() {
+        console.log("Olá Sou método da classe mãe");
+    }
 }
 exports.default = Animal;
 class Cachorro extends Animal {
     constructor(type, name, age, breed) {
-        super();
+        super(name, type, age); //Método que herda todos os atributos da class que estamos estendendo
+        this.breed = breed;
+    }
+    communicate() {
+        console.log('Sou da classe filha, classe cachorro');
     }
 }
+class Pessoa extends Animal {
+    constructor(type, name, age, color) {
+        super(name, type, age);
+        this.color = color;
+    }
+    communicate() {
+        console.log('Sou da classe filha, classe Pessoa');
+    }
+}
+let c1 = new Cachorro("Cachorro", "Marley", 2, "poodle");
+c1.communicate();
+let p1 = new Pessoa("Humano", "Kleber", 19, "Branco");
+p1.communicate();
--- animal.ts
+++ animal.ts
@@ -1,20 +1,48 @@
 export default class Animal {
-    type: string;
-    name: string;
-    age: number;
+  type: string;
+  name: string;
+  age: number;
 
-    constructor(type: string, name: string, age: number){
-        this.name = name;
-        this.type = type;
-        this.age = age;
-    }
+  constructor(type: string, name: string, age: number) {
+    this.name = name;
+    this.type = type;
+    this.age = age;
+  }
+
+  public communicate(): void {
+    console.log("Olá Sou método da classe mãe");
+  }
 }
 
 class Cachorro extends Animal {
-    breed: string;
+  breed: string;
 
-    constructor(type: string, name: string, age: number, breed: string){
-        super()
-    }
+  constructor(type: string, name: string, age: number, breed: string) {
+    super(name, type, age); //Método que herda todos os atributos da class que estamos estendendo
+    this.breed = breed;
+  }
 
+  public communicate(): void {
+      console.log('Sou da classe filha, classe cachorro')
+  }
 }
+
+class Pessoa extends Animal {
+  color: string;
+
+  constructor(type: string, name: string, age: number, color: string) {
+    super(name, type, age);
+
+    this.color = color;
+  }
+
+  public communicate(): void {
+    console.log('Sou da classe filha, classe Pessoa')
+}
+}
+
+let c1 = new Cachorro("Cachorro", "Marley", 2, "poodle");
+c1.communicate();
+
+let p1 = new Pessoa("Humano", "Kleber", 19, "Branco");
+p1.communicate();
